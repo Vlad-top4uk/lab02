04636cff924934c7a0d7e447fa2b701739faf6d1
Vlad-top4uk <105594241+Vlad-top4uk@users.noreply.github.com>
2022-12-22 15:59:22 +0200

    Add files via upload

---
+++ Lab02-Kosenko/Lab02-Kosenko/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Lab02_Kosenko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_P = new System.Windows.Forms.TextBox();
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.textBox_C = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_XOR = new System.Windows.Forms.Button();
+            this.button_clean = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_C2 = new System.Windows.Forms.TextBox();
+            this.textBox_Key2 = new System.Windows.Forms.TextBox();
+            this.textBox_P2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_P
+            // 
+            this.textBox_P.Location = new System.Drawing.Point(123, 37);
+            this.textBox_P.Name = "textBox_P";
+            this.textBox_P.Size = new System.Drawing.Size(100, 22);
+            this.textBox_P.TabIndex = 0;
+            this.textBox_P.TextChanged += new System.EventHandler(this.textBox_P_TextChanged);
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.Location = new System.Drawing.Point(123, 65);
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Key.TabIndex = 1;
+            // 
+            // textBox_C
+            // 
+            this.textBox_C.Location = new System.Drawing.Point(123, 93);
+            this.textBox_C.Name = "textBox_C";
+            this.textBox_C.Size = new System.Drawing.Size(100, 22);
+            this.textBox_C.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "P";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "C";
+            // 
+            // button_XOR
+            // 
+            this.button_XOR.Location = new System.Drawing.Point(229, 65);
+            this.button_XOR.Name = "button_XOR";
+            this.button_XOR.Size = new System.Drawing.Size(75, 23);
+            this.button_XOR.TabIndex = 6;
+            this.button_XOR.Text = "XOR";
+            this.button_XOR.UseVisualStyleBackColor = true;
+            this.button_XOR.Click += new System.EventHandler(this.button_XOR_Click);
+            // 
+            // button_clean
+            // 
+            this.button_clean.Location = new System.Drawing.Point(138, 121);
+            this.button_clean.Name = "button_clean";
+            this.button_clean.Size = new System.Drawing.Size(85, 23);
+            this.button_clean.TabIndex = 7;
+            this.button_clean.Text = "Прибрати";
+            this.button_clean.UseVisualStyleBackColor = true;
+            this.button_clean.Click += new System.EventHandler(this.button_clean_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(421, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Key";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(424, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "P";
+            // 
+            // textBox_C2
+            // 
+            this.textBox_C2.Location = new System.Drawing.Point(315, 37);
+            this.textBox_C2.Name = "textBox_C2";
+            this.textBox_C2.Size = new System.Drawing.Size(100, 22);
+            this.textBox_C2.TabIndex = 11;
+            // 
+            // textBox_Key2
+            // 
+            this.textBox_Key2.Location = new System.Drawing.Point(315, 66);
+            this.textBox_Key2.Name = "textBox_Key2";
+            this.textBox_Key2.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Key2.TabIndex = 12;
+            // 
+            // textBox_P2
+            // 
+            this.textBox_P2.Location = new System.Drawing.Point(315, 94);
+            this.textBox_P2.Name = "textBox_P2";
+            this.textBox_P2.Size = new System.Drawing.Size(100, 22);
+            this.textBox_P2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 199);
+            this.Controls.Add(this.textBox_P2);
+            this.Controls.Add(this.textBox_Key2);
+            this.Controls.Add(this.textBox_C2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_clean);
+            this.Controls.Add(this.button_XOR);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_C);
+            this.Controls.Add(this.textBox_Key);
+            this.Controls.Add(this.textBox_P);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_P;
+        private System.Windows.Forms.TextBox textBox_Key;
+        private System.Windows.Forms.TextBox textBox_C;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_XOR;
+        private System.Windows.Forms.Button button_clean;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_C2;
+        private System.Windows.Forms.TextBox textBox_Key2;
+        private System.Windows.Forms.TextBox textBox_P2;
+    }
+}
+
---
+++ Lab02-Kosenko/Lab02-Kosenko/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02_Kosenko
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_XOR_Click(object sender, EventArgs e)
+        {
+            string strP = textBox_P.Text;
+            int intP = Convert.ToInt32(strP, 16);
+            string strKey = textBox_Key.Text;
+            int intKey = Convert.ToInt32(strKey, 16);
+            int intC = intP ^ intKey;
+            string strC = Convert.ToString(intC, 16);
+            textBox_C.Text = strC;
+            string strP2 = textBox_P2.Text;
+            int intP2 = Convert.ToInt32(strP2, 2);
+            string strKey2 = textBox_Key2.Text;
+            int intKey2 = Convert.ToInt32(strKey2, 16);
+            int intC2 = intP2 ^ intKey2;
+            string strC2 = Convert.ToString(intC2, 16);
+            textBox_C2.Text = strC2;
+        }
+
+        private void button_clean_Click(object sender, EventArgs e)
+        {
+            textBox_P.Text = "" ;
+            textBox_Key.Text = "";
+            textBox_C.Text = "";
+            textBox_P2.Text = "";
+            textBox_Key2.Text = "";
+            textBox_C2.Text = "";
+        }
+
+        private void textBox_P_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
